5b220b5d9b238cfaff60045f6b9498d83ccdd22e
Greywood2003 <144985223+Greywood2003@users.noreply.github.com>
2025-02-17 11:51:41 +0300

    New structure

---
+++ user/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoginPage</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="logo">
+            <img src="./Images/Akado Logo - Black with Transparent Background - 5000x5000.png" alt="Akado Threads Logo">
+        </div>
+         <div class="login-text">
+        <p>Login to Your Account </p>
+        </div>
+        <div class="login-form">
+          
+        <form action="">
+         <input type="text" name="Username/email" placeholder="Username or Email" id="user">
+         <br>
+         <input type="password" name="password" placeholder="Password" id="pass">
+         <br><br>
+         
+         <input type="submit" name="Login" value="Login" id="log_in">
+    
+         <br><br>
+        </form>
+        </div>
+        <div class="or-text">
+            <p>Or sign in with</p>
+        </div>
+        <div class="other-logins">
+            <img src="./Images/New-Google-Logo-497x500.jpg" alt="Google Logo">
+            <img src="./Images/facebook-logo_1080029-107.avif" alt="facebook logo">  
+            <div class="imagex">
+                <img src="./Images/1725374683twitter-x-logo.png" alt="X twitter logo">
+            </div>
+        
+    </div>
+    <div class="login_signup">
+        <p> Don't have an account? <a href="./Signup.html">Signup</a></p>
+    </div>
+    </div>
+</body>
+</html>
